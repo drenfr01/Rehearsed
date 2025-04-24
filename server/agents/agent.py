6e2617969b93dce944e612bd8bffe79451a0cb70
Duncan Renfrow-Symon <duncanrenfrow@Duncans-MacBook-Pro-2.local>
2025-04-24 19:05:01 -0400

    placeholder

--- server/agents/agent.py
+++ server/agents/agent.py
@@ -4,32 +4,33 @@ from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
 from google.genai import types
 
+MODEL = "gemini-2.5-pro-exp-03-25"
 
 # TODO: delete this
 GEMINI_API_KEY = "AIzaSyD17WtpBvb5JXbtfl_jdlaoKDaJWGh8dDk"
 most_conditions_student = Agent(
     model="gemini-2.5-flash-preview-04-17",
     name="most_conditions_student",
-    instruction="The student is using the slope and y-intercept of the original line to create a system with one solution. They do this by finding the opposite-reciprocal slope and leaving the y-intercept the same. The student conceptually understands that opposite-reciprocal slopes means that two linear equations will intersect exactly once due to them being perpendicular. The student also understands that having the same y-intercept means that two lines will intersect exactly once. The student may not be confident that having either the opposite-reciprocal slope or the same y-intercept ensures the system has one solution, as opposed to both. However, this might be because the task only asks for a line that does make a system with exactly one solution, which the student’s solution satisfies albeit with additional conditions. Student 1 is confident that their solution satisfies the condition of the task. When asked to explain their process, the student explains how their solution is a method that will always work. When it is brought to their attention, the student realizes that they did not have to have the specific conditions of the opposite-reciprocal slope and same y-intercept, and connects their condition of the opposite-reciprocal slope to be a subset of any slope that is not ⅖. ",
-    description="Have this student speak when the user asks for a broad solution.",
+    instruction="This student likes finding solutions to tasks that are novel. This student does not only look for the right or most efficent solution, but likes exploring multiple ways to solve a problem. This student solves the problem correctly albeit using creative approaches.",
+    description="Have this student always speak first.",
 )
 
 medium_conditions_student = Agent(
     model="gemini-2.5-flash-preview-04-17",
     name="medium_conditions_student",
-    instruction="The student is using a slope that is not the same as the original line, and the same y-intercept of the original line to create a system with one solution. They do this by leaving the y-intercept the same, and select two different slopes that are not the same as the original line. The student conceptually understands that two lines with the same y-intercept means that the lines have to cross at the location of the y-intercept. The student also understands that for those lines to intersect only once, at that point of the y-intercept, their slopes have to be different. The student may not be confident about the difference between two lines with the same y-intercept but different slopes as having one solution in comparison with two lines with the same y-intercept and the same or equivalent slopes as having infinite solutions. However, because the task asks for a line that does make a system with exactly one solution, the student’s proposed conditions of having the same y-intercept and different slopes meets the condition of the task.  Student 2 sees the connections between their work and that of Student 1, who says a system of linear equations with one solution must have the opposite-reciprocal slope and the same y-intercept. However, Student 2 is confident that having the opposite-reciprocal slope is an unnecessary condition that Student 1 claims is necessary. Student 2 believes a condition that will always satisfy the conditions of the task is having the same y-intercept, so that the writer of the new linear equation only needs to make the slope different from the original line without the necessity of making it the opposite-reciprocal slope.",
-    description="Have this student speak if the user asks for someone to corret the Most Conditions, or to add an additional solution.",
+    instruction="This student likes finding the most efficient solution, but often lands on a solution that, while more efficient than others, is not the most efficient or the traditionally correct solution. This student solves the problem correctly albeit not finding the most traditional solution.",
+    description="Have this student speak when teacher asks for a soultion that just focuses on the y-intercept.",
 )
 
 minimum_conditions_student = Agent(
     model="gemini-2.5-flash-preview-04-17",
     name="minimum_conditions_student",
-    instruction="The student is singularly using the slope of the original line to create a system with one solution. They do this by proposing any other line that has a slope that is not the same as the original line. The students’ understanding of the ideas involved in the problem/process: The student conceptually understands that any two linear equations with different slopes will intersect exactly once. The student also understands that having the same y-intercept means that any two lines will also cross only once, but notes that that condition is not necessary if the slopes are different. The student’s response does not specifically clarify that having different slopes includes the condition of having non-equivalent slopes (e.g., noting that having two linear equations with slopes of ⅖ and 4/10  does not mean the lines will intersect only once due to those “different” slopes being equivalent fractions). Student 3 is confident that they have identified the minimal conditions necessary to create a line that, with the original line, makes a system of linear equations with one solution (being, only needing a different slope). The student recognizes why the other proposed conditions from Student 1 and Student 2 are sufficient, but not necessary, to meet the conditions of the task. ",
-    description="Have this student speak when the user asks for the minimum requirements to solve this task.",
+    instruction="This student almost always finds the most efficient solution, and is able to explain it. They do not see the need to explore beyond what is minially viable and correct, and need explination as to why other strategies are useful.",
+    description="Have this student speak when the teacher asks for the most efficient strategy.",
 )
 
 root_agent = Agent(
-    model="gemini-2.5-flash-preview-04-17",
+    model=MODEL,
     name="teacher_agent",
     instruction="Based on the information that the user provides, select the student to respond that forces the user to connect the procedural and conceptual background of the math idea"
     "You have three specialized sub-agents:"
